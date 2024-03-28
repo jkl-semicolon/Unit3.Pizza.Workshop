2ec3667935da85df623de9dc8fcadd174f4f06d8
jkl <alexwang925@gmail.com>
2024-03-28 17:34:18 -0300

    final edits

--- src/App.jsx
+++ src/App.jsx
@@ -32,7 +32,7 @@ function App() {
  
   return (
     <>
-      <h1>Pizza List</h1>
+      <h1>🍕Pizza List🍕</h1>
       <InputForm pizzaList={pizzaList} setPizzaList={setPizzaList}/>
       {pizzaList.length ? <Pizzas pizzaList={pizzaList}/> : <h3>No Pizzas Yet!</h3>}
     </>
@@ -40,10 +40,3 @@ function App() {
 };
 
 export default App;
-
-
-// Create a pizza website with the following features:
-// a list of pizzas that we already sell
-// a form that allows the user to add a new pizza to the list
-// the form should take in a name for the pizza and one ingredient
-// EXTRA: the form takes in multiple ingredients
